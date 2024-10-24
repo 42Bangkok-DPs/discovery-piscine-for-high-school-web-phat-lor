6cbcd80f8fef857b52fdb25400ffe68ae0281445
Phat Lorthammakun <153535555+phat-lor@users.noreply.github.com>
2024-10-24 19:03:49 +0700

    push

--- rush/assets/js/portfolio.js
+++ rush/assets/js/portfolio.js
@@ -4,21 +4,21 @@ const data = {
 			title: "Pat's Site",
 		},
 		landing: {
-			title: "Phat Lorthammakun",
+			title: "Phat Lorthammakun ",
 			subtitle: "Fullstack Developer",
-			image: "assets/images/pat.jpg",
+			image: "assets/img/phat.png",
 		},
 		about: {
 			title: "About Me",
-			subtitle: "I'm a fullstack developer from Thailand.",
-			description: "I'm a full...",
+			description:
+				"Hello! I'm a high school student and full-stack developer! Since 2021, I have built websites, backends and games. I'm always looking for new projects to work on and new things to learn.",
 
 			skills: {
 				title: "Skills",
 				subtitle: "I have experience with the following technologies:",
 				skills: [
 					{ name: "HTML", icon: "https://skillicons.dev/icons?i=html" },
-					{ name: "CSS", icon: "https://skillicons.dev/icons?i=CSS" },
+					{ name: "CSS", icon: "https://skillicons.dev/icons?i=css" },
 				],
 			},
 
@@ -73,10 +73,47 @@ function applyData() {
 	}
 
 	try {
-		document.title = port.title;
+		document.title = port.metadata.title;
 
-		showContent();
-		hideLoadingModal();
+		// <div class="flex-row flex gap-1 items-center h-fit">
+		// 							<img
+		// 								src="https://skillicons.dev/icons?i=html"
+		// 								alt="HTML"
+		// 								class="w-7 h-7"
+		// 							/>
+		// 							<p>HTML</p>
+		// 						</div>
+
+		const landingTitle = document.getElementById("landing-title");
+		landingTitle.innerText = port.landing.title;
+
+		const landingSubtitle = document.getElementById("landing-subtitle");
+		landingSubtitle.innerText = port.landing.subtitle;
+
+		const landingImage = document.getElementById("landing-image");
+		landingImage.src = port.landing.image;
+
+		const skillsContent = document.getElementById("skills-content");
+		const skills = port.about.skills.skills;
+		skills.forEach((skill) => {
+			const div = document.createElement("div");
+			div.classList.add("flex-row", "flex", "gap-1", "items-center", "h-fit");
+			div.innerHTML = `<img src="${skill.icon}" alt="${skill.name}" class="w-7 h-7" />
+				<p>${skill.name}</p>`;
+			skillsContent.appendChild(div);
+		});
+
+		const educationContent = document.getElementById("education-content");
+
+		const aboutContent = document.getElementById("about-content");
+		const p = document.createElement("p");
+		p.innerText = port.about.description;
+		aboutContent.appendChild(p);
+
+		setTimeout(() => {
+			showContent();
+			hideLoadingModal();
+		}, 1000);
 	} catch (e) {
 		console.error(e);
 		showErrorModal();
